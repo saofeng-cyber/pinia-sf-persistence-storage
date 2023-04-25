eff10e9d9ad88fe6b55ad8779924b2fbed9e6331
sunminggang <942391953@qq.com>
2023-04-25 17:27:46 +0800

    fix: 修改包名

--- vite.config.ts
+++ vite.config.ts
@@ -15,9 +15,9 @@ export default defineConfig({
       // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, "src/index.ts"),
       formats: ["cjs", "umd"],
-      name: "PiniaSfPersistenceStorage",
+      name: "PiniaPersistenceSfStorage",
       // the proper extensions will be added
-      fileName: "pinia-sf-persistence-storage",
+      fileName: "pinia-persistence-sf-storage",
     },
   },
 });
